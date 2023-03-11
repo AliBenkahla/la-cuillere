0175f1aa6fefffb09d28c8c7db8e0bdfbcd8de08
Younes <younesmedj@gmail.com>
2023-03-11 20:13:18 +0100

    Solution for form

--- src/app/restaurant-list-test/restaurant-list-test.component.ts
+++ src/app/restaurant-list-test/restaurant-list-test.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RestaurantArrayService } from '../services/restaurant-array.service';
 import { Restaurants } from '../models/restaurants.model';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-
-
-
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-restaurant-list-test',
@@ -13,33 +10,37 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./restaurant-list-test.component.scss'],
 })
 export class RestaurantListTestComponent implements OnInit {
+  urlDpt!: string;
+  restaurantArray!: Restaurants[];
+  Data: Array<any> = [
+    { name: 'Tunisiens', value: 'tunisiens' },
+    { name: 'Marocains', value: 'marocains' },
+    { name: 'Francais', value: 'francais' },
+  ];
+  form: FormGroup = this.formBuilder.group({});
 
-  urlDpt! : string;
-  restaurantArray! : Restaurants[];
-  form! : FormGroup;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private restaurantArrayService: RestaurantArrayService,
+    private formBuilder: FormBuilder
+  ) {
+    this.Data.forEach((option) => {
+      this.form.addControl(option.value, new FormControl(false));
+    });
+  }
 
-
-  constructor(  private route : ActivatedRoute,
-                private router : Router,
-                private restaurantArrayService: RestaurantArrayService,
-                private formBuilder: FormBuilder) {
-
-                }
-
-                submit() { 
-                 
-                  
-                  console.log(this.form.value);
-                 }
-
-
-               
+  submit() {
+    console.log(this.form.value);
+  }
 
   ngOnInit() {
     this.urlDpt = this.route.snapshot.params['dpt'];
     console.log(this.urlDpt);
 
-    this.restaurantArray = this.restaurantArrayService.getRestaurantsDpt(this.urlDpt);
+    this.restaurantArray = this.restaurantArrayService.getRestaurantsDpt(
+      this.urlDpt
+    );
     console.log(this.restaurantArray);
   }
 
@@ -48,9 +49,8 @@ export class RestaurantListTestComponent implements OnInit {
     console.log(this.restaurantArray);
   }
 
-  onSortRestaurantsDescendant(){
+  onSortRestaurantsDescendant() {
     this.restaurantArray = this.restaurantArray.sort((b, a) => a.note - b.note);
     console.log(this.restaurantArray);
   }
-
 }
